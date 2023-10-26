2dbd3feb07d7ef60a76b321aabd571d5b0a8ef22
sannyralph <sannylauren@gmail.com>
2023-10-25 20:12:21 -0400

    hero page mobile aliged

--- src/hero/hero.jsx
+++ src/hero/hero.jsx
@@ -1,7 +1,9 @@
 import "./hero.css";
-import star from "./../assets/star.png"
-import star1 from "./../assets/star2.png"
-import mann from "./../assets/man.png"
+import star from "./../assets/star0.png"
+import star1 from "./../assets/star01.png"
+import chain from "./../assets/chain.png"
+import starIcon from "./../assets/lil-star.png"
+import light from "./../assets/Image1.png"
 import Button from '@mui/material/Button';
 // import image01 from "./../assets/Image1.png"
 
@@ -12,18 +14,18 @@ const Home = () => {
                 <h4>Igniting a Revolution in HR Innovation</h4>
                 <img src={star}  alt="starPhoto" className="starPhoto"/>
                 <img src={star1}  alt="starPhoto" className="starPhoto"/>
-                <div>
-                    <h3>getlinked  Tech</h3>
-                    <h3>hackathon 1.0</h3>
+                <div className="subhead">
+                   <span>getlinked  Tech</span><br/>
+                   <span>hackathon<span>1.0</span></span><span><img src={chain} alt="chain-icon"/></span><span><img src={starIcon} alt="chain-icon"/></span>
                 </div>
-                <div>
-                    <p>Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize</p>
+                <div className="paragraphs">
+                    <p>Participate in getlinked tech Hackathon <br/> 2023 stand a chance to win a Big prize</p>
                    <span><Button variant="contained" className='button' >Contained</Button></span> <span> <img src={star1}  alt="starPhoto" className="starPhoto"/></span>
                 </div>
                 <div><span>00H</span><span>00M</span><span>00S</span></div>
             </div>
-            <div>
-                <img src={mann} alt="man-with-smartglassess"  style={{width:'100%', height: '100%'}} />
+            <div id="background-image">
+                <img src={light} alt="man-with-smartglassess"  style={{width:'100%', height: '100%'}}  />
             </div>                           
         </section>                                                              
     );
