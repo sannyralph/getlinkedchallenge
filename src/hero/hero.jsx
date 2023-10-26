2607593d25b27b3dfbcf90e8795194676f32b114
sannyralph <sannylauren@gmail.com>
2023-10-25 20:59:35 -0400

    timer fixed

--- src/hero/hero.jsx
+++ src/hero/hero.jsx
@@ -16,13 +16,15 @@ const Home = () => {
                 <img src={star1}  alt="starPhoto" className="starPhoto"/>
                 <div className="subhead">
                    <span>getlinked  Tech</span><br/>
-                   <span>hackathon<span>1.0</span></span><span><img src={chain} alt="chain-icon"/></span><span><img src={starIcon} alt="chain-icon"/></span>
+                   <span>hackathon <span id="subheadDigit">1.0</span></span><span><img src={chain} alt="chain-icon"/></span><span><img src={starIcon} alt="chain-icon"/></span>
                 </div>
                 <div className="paragraphs">
                     <p>Participate in getlinked tech Hackathon <br/> 2023 stand a chance to win a Big prize</p>
                    <span><Button variant="contained" className='button' >Contained</Button></span> <span> <img src={star1}  alt="starPhoto" className="starPhoto"/></span>
                 </div>
-                <div><span>00H</span><span>00M</span><span>00S</span></div>
+                <div className="timer" >
+                    <span className="timer-digits">00<span className="timer-letter">H</span></span><span className="timer-digits">00<span className="timer-letter">M</span></span><span className="timer-digits">00<span className="timer-letter">S</span></span>
+                </div>
             </div>
             <div id="background-image">
                 <img src={light} alt="man-with-smartglassess"  style={{width:'100%', height: '100%'}}  />
